1ab4498bc9e36855fcbf02cd65c5643e163faad1
Troy Berhow <troyberhow@u.boisestate.edu>
2025-03-06 18:03:23 -0700

    Filled out hashobject and started work on hashtable.java

--- HashObject.java
+++ HashObject.java
@@ -1,3 +1,31 @@
 public class HashObject {
-    
+    Object object;
+    Object key;
+    int frequency;
+    int probeCount;
+
+    public HashObject(Object o, Object k) {
+        object = o;
+        key = k;
+        frequency = 0;
+        probeCount = 0;
+    }
+
+    public boolean equals(Object compare) {
+        if (key.equals(compare)) {
+            frequency++;
+            return true;
+        }
+        return false;
+    }
+
+    public Object getKey() {
+        return key;
+    }
+
+    public String toString() {
+        String str = "";
+        str += object + " key value: " + key + " duplicates: " + frequency + " probe count: " + probeCount;
+        return str;
+    }
 }
--- Hashtable.java
+++ Hashtable.java
@@ -1,3 +1,23 @@
 public abstract class Hashtable {
     protected HashObject[] table;
+
+    // public Enum status {
+    //     Ocupided, Del, 
+    // }
+
+    public Hashtable(int size) {
+        table = new HashObject[size];
+    }
+
+    public abstract void insert();
+
+    public abstract int find();
+
+    public abstract void delete();
+
+    // public String toString() {
+    //     for (int i=0; i<table.length; i++) {
+    //         if (table[i])
+    //     }
+    // }
 }
