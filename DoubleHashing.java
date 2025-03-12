4bfe031665d1bc5d99cd45f719ee268ac8969468
Troy Berhow <troyberhow@u.boisestate.edu>
2025-03-11 23:29:12 -0600

    Written out DoubleHashing.java

--- DoubleHashing.java
+++ DoubleHashing.java
@@ -1,3 +1,49 @@
-public class DoubleHashing {
+public class DoubleHashing extends Hashtable {
+
+    int probe = 0;
+    int m;
+    int n;
+
+    public DoubleHashing(int size, int bigPrime, int smallPrime) {
+            super(size);
+            m = bigPrime;
+            n = smallPrime;
+        }
+    
+        @Override
+    public void insert(HashObject[] hashTable, int key, Object value) throws HashException {
+        HashObject stock = new HashObject(value, key);
+        for(int i=0; i<hashTable.length; i++) {
+            probe = ((key % m) + i * (1 + (key % (m - 2)) % m)) % m;
+            if (hashTable[probe].getStatus() == 0) {
+                hashTable[probe].increaseDupCount();
+            }
+            if (hashTable[probe].getStatus() != 0) {
+                hashTable[probe] = stock;
+            }
+        }
+    }
+
+    @Override
+    public int find(HashObject[] hashTable, int key) {
+        for (int i=0; i<hashTable.length; i++) {
+            probe = ((key % m) + i * (1 + (key % (m - 2)) % m)) % m;
+            if (hashTable[probe].getKey().equals(key) && hashTable[probe].getStatus() == 0) {
+                return probe;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void delete(HashObject[] hashTable, int key) {
+        for (int i=0; i<hashTable.length; i++) {
+            probe = ((key % m) + i * (1 + (key % (m - 2)) % m)) % m;
+            if (hashTable[probe].getKey().equals(key)) {
+                hashTable[probe].setStatus(HashObject.status.DEL);
+            }
+        }
+        System.out.println("key not in table");
+    }
     
 }
--- LinearProbing.java
+++ LinearProbing.java
@@ -27,7 +27,7 @@ public class LinearProbing extends Hashtable {
     @Override
     public int find(HashObject[] hashTable, int key) {
         for (int i=0; i<hashTable.length; i++) {
-            probe = (key + i) % m;
+            probe = ((key % m) + i) % m;
             if (hashTable[probe].getKey().equals(key)) {
                 return probe;
             }
@@ -38,8 +38,9 @@ public class LinearProbing extends Hashtable {
     @Override
     public void delete(HashObject[] hashTable, int key) {
         for (int i=0; i<hashTable.length; i++) {
-            if (hashTable[i].getKey().equals(key)) {
-                hashTable[i].setStatus(HashObject.status.DEL);
+            probe = ((key % m) + i) % m;
+            if (hashTable[probe].getKey().equals(key)) {
+                hashTable[probe].setStatus(HashObject.status.DEL);
             }
         }
         System.out.println("key not in table");
