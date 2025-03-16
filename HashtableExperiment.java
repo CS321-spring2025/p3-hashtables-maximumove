b6d93ec8cb2c89cb8694999f8192f9b2cc8e2cbf
Troy Berhow <troyberhow@u.boisestate.edu>
2025-03-16 16:21:05 -0600

    Add command line validation to driver class

--- HashtableExperiment.java
+++ HashtableExperiment.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 import java.util.Hashtable;
 import java.util.Random;
 
@@ -17,14 +18,28 @@ public class HashtableExperiment {
         }
         int dataSource = Integer.parseInt(args[0]);
         long loadFactor = Integer.parseInt(args[1]);
+        int debugLevel = 0;
         if(args.length == 3) {
-            int debugLevel = Integer.parseInt(args[2]);
+            debugLevel = Integer.parseInt(args[2]);
         }
 
         int size = TwinPrimeGenerator.GenereateTwinPrimes(30, 500);
 
         LinearProbing testLinear = new LinearProbing(100, size);
         DoubleHashing testDouble = new DoubleHashing(100, size, size-2);
+
+        if (dataSource < 1 || dataSource > 3) {
+            System.out.println("Error: Invalid data source. Please input integer value between 1 and 3");
+            return;
+        }
+        if (loadFactor < 0 || loadFactor >= 1) {
+            System.out.println("Error: Invalid load factor. Please input decimal value between 0 and 1");
+            return;
+        }
+        if (debugLevel < 0 || debugLevel > 2) {
+            System.out.println("Error: Invalid debug level. Please input integer value between 0 and 2");
+            return;
+        }
 
         if (dataSource == 1) {
             Random rand = new Random();
@@ -33,6 +48,19 @@ public class HashtableExperiment {
                 int key = obj.hashCode();
                 testLinear.insert(key, obj);
             }
+            for (int i=0; i < (100*loadFactor); i++) {
+                Object obj = rand.nextInt();
+                int key = obj.hashCode();
+                testDouble.insert(key, obj);
+            }
+        }
+
+        if (dataSource == 2) {
+            // long current = Date().getTime();
+        }
+
+        if (dataSource == 3) {
+
         }
     }
 }
